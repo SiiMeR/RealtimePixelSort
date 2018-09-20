9c9e069b5ad3468dd1631d9598f51e5b5efdeaea
Siim Raudsepp <siimraudsepp@msn.com>
2018-09-20 17:21:02 +0300

    Some experiments with compute shaders

--- Assets/Scripts/Pixelsort.cs
+++ Assets/Scripts/Pixelsort.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class Pixelsort : MonoBehaviour
 {
 
     public ComputeShader CSImagePixelSort;
-    
+
+    private int kernelID;
     // Start is called before the first frame update
     void Start()
     {
-        
+        kernelID = CSImagePixelSort.FindKernel("CSMain");
     }
 
     // Update is called once per frame
@@ -21,6 +23,17 @@ public class Pixelsort : MonoBehaviour
 
     private void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
-        Graphics.Blit(src,dest);
+        var sortingTexture = RenderTexture.GetTemporary(src.width, src.height, src.depth);
+        sortingTexture.enableRandomWrite = true;
+        
+        sortingTexture.Create();
+
+        CSImagePixelSort.SetTexture(kernelID, "Source", src);
+        CSImagePixelSort.SetTexture(kernelID, "Result", sortingTexture);
+        CSImagePixelSort.Dispatch(kernelID,  (sortingTexture.width + 7) / 8, (sortingTexture.height + 7) / 8, 1);
+        
+        Graphics.Blit(sortingTexture,dest);
+        
+        RenderTexture.ReleaseTemporary(sortingTexture);
     }
 }
