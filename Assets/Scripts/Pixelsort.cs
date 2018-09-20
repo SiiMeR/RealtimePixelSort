db027e9c7b50d83d4e99695d84a78933a881ffe8
Siim Raudsepp <siimraudsepp@msn.com>
2018-09-20 16:06:07 +0300

    Set up Unity project and .gitignore

---
+++ Assets/Scripts/Pixelsort.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pixelsort : MonoBehaviour
+{
+
+    public ComputeShader CSImagePixelSort;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src,dest);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # RealtimePixelSort
-A repository to explore pixel sorting in realtime. Contains a few interactive demos with some of my ideas. Done using Unity.
+The main idea of how it works is that the image is post processed after rendering using Compute shaders. Per object pixelsorting may have to be done using regular fragment/vertex shaders.
