ce3117f697174ebf59b7f6a58162fb99b83152e4
Siim Raudsepp <siimraudsepp@msn.com>
2018-09-21 17:35:33 +0300

    Added sorting algorithm

--- Assets/Scripts/Pixelsort.cs
+++ Assets/Scripts/Pixelsort.cs
@@ -9,6 +9,8 @@ public class Pixelsort : MonoBehaviour
     public ComputeShader CSImagePixelSort;
 
     private int kernelID;
+
+    private float iterations;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,8 +30,10 @@ public class Pixelsort : MonoBehaviour
         
         sortingTexture.Create();
 
+        CSImagePixelSort.SetFloat("uIteration", iterations++);
         CSImagePixelSort.SetTexture(kernelID, "Source", src);
         CSImagePixelSort.SetTexture(kernelID, "Result", sortingTexture);
+        CSImagePixelSort.SetVector("Resolution", new Vector4(Screen.currentResolution.width, Screen.currentResolution.height));
         CSImagePixelSort.Dispatch(kernelID,  (sortingTexture.width + 7) / 8, (sortingTexture.height + 7) / 8, 1);
         
         Graphics.Blit(sortingTexture,dest);
